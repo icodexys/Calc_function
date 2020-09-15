e51e331213d70f78cc16a5932249632dbde9fb99
Jheff Mat <jheff.at@gmail.com>
2020-09-14 23:24:05 -0700

    Update Calc_Ct.py
    Exponential notation added

--- Calc_Ct.py
+++ Calc_Ct.py
@@ -3,18 +3,19 @@ def mid(s,pos,amout):
     return s[pos:pos+amout]
 def calc_ct(expx):
     group_num=""; express=[];oper="";acomulador=0.0;
-    #Here will convert from string to List type spliting groups number and operators +-*/
+    #Here will convert from string to List spliting groups numbers and operators +-*/
     for i in expx:
-        if i in string.ascii_letters+"=$#@!&~_)(^[]{}|?><":return "Error operation"  
+        if i in string.ascii_letters+"=$#@!&~_)([]{}|?><":return "Error operation"  
         if i in ".0123456789":
             group_num+=i
-        elif i in "+-*/":
+        elif i in "+-*/^":
             if len(group_num)>0:
                 express+=[group_num]
                 group_num=""
             express+=[i]   
     express+=[group_num]
-    #will just return with a single value if exist otherwise return an error
+    print(express)
+    #will just return  a single value if exist otherwise return an error
     if len(express)<=2: 
         try:
             return float(expx)
@@ -25,6 +26,21 @@ def calc_ct(expx):
     
     #if the first item from the list express equal '-' then we'll add new item '0'....
     if express[0]=="-" and len(express)>=4:express=["0"]+express
+    
+    #if exist Exponential notation ^ then execute the this operation......
+    while "^" in express:
+        for y in range(0,len(express),2):
+            try:
+                catchx=mid(express,y,3)
+                if "^" in catchx:
+                    express.pop(y)
+                    express.pop(y)
+                    express.pop(y)
+                    p=float(catchx[0])**float(catchx[2])
+                    express.insert(y,str(p))           
+            except:
+                return "Invalid expression"
+    
     #if exist any item like '*' or '/' then execute the multiplication and division...
     while "*" in express or "/" in express:
         for y in range(0,len(express),2):
@@ -54,12 +70,9 @@ def calc_ct(expx):
     return acomulador
 
 
-
-
-
 resultado=0.0
 #by iCOdexys-------*-*-*-*-
-print("Calculator in one line with /*-+")
+print("Calculator in one line with /*-+^")
 while  resultado!=0.1:
     resultado=calc_ct(input("Calc: ")) 
     print(resultado)
